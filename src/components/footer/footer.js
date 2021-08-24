a4ef61568512c22af8fdd1790a92ebd0afd7f10d
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2021-08-24 19:09:01 +0300

    feat: add footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -5,8 +5,34 @@ import data from './footer.data';
 import FooterLogo from 'assets/logo.svg';
 
 export default function Footer() {
+  const renderMenuItem = (item) => (
+    <Link
+      path={item.path}
+      key={item.label}
+      sx={styles.footer.link}
+    >
+      {item.label}
+    </Link>
+  );
+
   return (
-    <h1>Footer</h1>
+    <footer sx={styles.footer}>
+      <Container>
+        <Box sx={styles.footer.footerBottomArea}>
+          <Link path="/">
+            <Image src={FooterLogo} />
+          </Link>
+          <Box sx={styles.footer.menus}>
+            <nav>
+              {data.menuItem.map(renderMenuItem)}
+            </nav>
+          </Box>
+          <Text sx={styles.footer.copyright}>
+            Copyright by {new Date().getFullYear()} Company Name
+          </Text>
+        </Box>
+      </Container>
+    </footer>
   );
 }
 
